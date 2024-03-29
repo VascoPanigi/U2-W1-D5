5b1b188e97c4f51811dc0857c1d2ac02d89909af
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-03-30 00:21:08 +0100

    M animation working

--- base_di_partenza/script.js
+++ base_di_partenza/script.js
@@ -29,15 +29,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  const randomNumber = Math.floor(Math.random() * opacityZeroElements.length);
-  const randomElement = opacityZeroElements[randomNumber];
+  //tentativo fallimentare
+  //   const randomNumber = Math.floor(Math.random() * opacityZeroElements.length);
+  //   const randomElement = opacityZeroElements[randomNumber];
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+  console.log(shuffleArray);
+
+  const shuffledArray = shuffleArray(opacityZeroElements);
+  let currentIndex = 0;
 
   const showM = () => {
-    if (randomElement.getAttribute("opacity") === "0") {
-      randomElement.setAttribute("opacity", "1");
+    const currentElement = shuffledArray[currentIndex];
+
+    if (currentElement.getAttribute("opacity") === "0") {
+      currentElement.setAttribute("opacity", "1");
     } else {
-      randomElement.setAttribute("opacity", "0");
+      currentElement.setAttribute("opacity", "0");
     }
+
+    currentIndex = (currentIndex + 1) % shuffledArray.length;
   };
 
   setInterval(showM, 200);
